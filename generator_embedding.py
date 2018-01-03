b294f98263659b500d5c849b38298b992ad1d31a
Arnaud Buisson <arnaud.buisson@hec.edu>
2018-01-03 13:55:26 +0100

    additionnal code

---
+++ generator_embedding.py
@@ -0,0 +1,133 @@
+import numpy as np
+from sklearn.datasets import make_classification, make_blobs
+from tools import SC
+
+def sample_spherical(npoints, ndim=3):
+    vec = np.random.randn(ndim, npoints)
+    vec /= np.linalg.norm(vec, axis=0)
+    return vec
+
+
+def generate_embedded_data(T, T_historical, n_users, n_products, d, d_reduced, method_users = 'blobs', method_products= 'blobs',
+                  n_class_products = None, n_class_users = 4, context_len = 3, context_emb = 5, emb_noise = 0.1,
+                  plot_emb = True, users_cluster_param = 0.05, products_cluster_param = 0.01):
+    '''
+    generates the sequence of {i_t,C_t} over time.
+
+
+    INPUT :
+    ***** FROM THE ARTICLE   *****
+    - T: number if time step
+    - n_users: number of users
+    - n_products: number of products
+    - d: number of features
+    - method_users: string giving the method for the generation of the users
+    - method_products: string giving the method for the generation of the products
+
+
+    *****  IMPLEMENTATION DETAILS   *****
+    - T_historical : number of previous timestep
+    - d_reduced : the size of the embeddings
+    - n_class_users : number of latent classes in the users
+    - n_class_products : number of latent classes in the products
+    - context_len: number of item in the context at each step
+    - context_emb : size of the context for historical data
+    - emb_noise : noise in the historical data
+    - plot_emb : whether to plot the spectral clustering of not
+    - users_cluster_param : distance between classes or std of the users clusters
+    - products_cluster_param : distance between classes or std of the product clusters
+
+    OUPUT:
+    A list
+        Output[0]: a matrix giving the vectors of the different users
+        Output[1]: a matrix giving the vectors of the different products
+        Output[2]: a matrix giving the embedded vectors of the different products
+        Output[3]:  list of lists, each list is the context at time t and is made of two elements:
+                    - indice of the user picked
+                    - array containing indices of the available products (context)
+
+    '''
+
+    # Create the users
+    if method_users == 'blobs':
+        users_params = {"n_samples": n_users, "n_features": d, "centers": sample_spherical(n_class_users, d).T,
+                        "cluster_std": users_cluster_param, "shuffle": True, "random_state": None}
+        [users, users_labels] = make_blobs(**users_params)
+
+    elif method_users == 'classification':
+        users_params = {"n_samples": n_users, "n_features": d, "n_informative": d,
+                        "n_repeated": 0, "n_classes": n_class_users, "n_clusters_per_class": 1,
+                        "flip_y": 0.0, "class_sep": users_cluster_param, "hypercube": None,
+                        "shift": 0.0, "scale": 1.0, "shuffle": True,
+                        "random_state": None}
+        [users, users_labels] = make_classification(**users_params)
+    else:
+        raise ValueError('Unknown method used for the generation of the users')
+
+    users = users / (np.sum(np.abs(users)**2, axis=-1)**(1./2))[:, np.newaxis]
+
+    # Create the products
+    if method_products == 'blobs':
+        if n_class_products:
+            products_params = {"n_samples": n_products, "n_features": d,
+                               "centers": sample_spherical(n_class_products, d).T,
+                               "cluster_std": products_cluster_param, "shuffle": True, "random_state": None}
+            [products, products_labels] = make_blobs(**products_params)
+        else:
+            raise ValueError('n_class_products should be specified to use the blobs method')
+
+    elif method_products == 'classification':
+        if n_class_products:
+            products_params = {"n_samples": n_products, "n_features": d,  "n_informative": d,
+                               "n_repeated": 0, "n_classes": n_class_products, "n_clusters_per_class": 1,
+                               "flip_y": 0.0, "class_sep": products_cluster_param, "hypercube": None,
+                               "shift": 0.0, "scale": 1.0, "shuffle": True, "random_state": None}
+            [products, products_labels] = make_classification(**products_params)
+        else:
+            raise ValueError('n_class_products should be specified to use the classification method')
+
+    elif method_products == 'One-Hot':
+        if d != n_products:
+            raise ValueError('When using One-Hot d and n_products should be equal')
+        else:
+            products = np.eye(d)
+
+    else:
+        raise ValueError('Unknown method used for the generation of the products')
+
+    products = products / (np.sum(np.abs(products)**2, axis=-1)**(1./2))[:, np.newaxis]
+
+    #Creation of embeddings :
+
+    hist_mat = np.zeros((d,n_users))
+
+    #We generate historical data
+    for t in range(T_historical):
+        i = np.random.randint(0, n_users)
+        user_selected = users[i,:]
+        item_number = np.random.randint(1,d)
+        idx = np.random.choice(d, context_emb, replace=False)
+        items_available = products[idx,:]
+        item_chosen = np.argmax(user_selected @ items_available.T + np.random.normal(0, emb_noise,(1,bound)))
+        item_chosen = idx[item_chosen]
+        hist_mat[item_chosen,i] += 1
+
+    hist_mat = hist_mat / (np.sum(np.abs(hist_mat)**2,axis=-1)**(1./2))[:, np.newaxis]
+
+    #We perform spectral clustering
+    SC_args = {'X' : hist_mat.T, 'sigma': 100, 'graph_type':'knn', 'thresh':3 , 'n_clusters': n_class_products, 'n_axis': d_reduced,
+              'nor_type':'nor', 'labels': products_labels, 'plot': plot_emb}
+
+    embedded_products = SC(**SC_args)
+    print("embedding produced")
+
+
+
+    # Creation of the learning sequences
+    output = []
+    for t in range(T):
+        i = np.random.randint(0, n_users)   # user picked
+        item_available = np.random.choice(n_products, context_len, replace=False)
+        output += [[i, item_available]]
+
+    return [np.matrix(users), products, embedded_products, output]
---
+++ tools.py
@@ -0,0 +1,117 @@
+
+
+def build_similarity(M, sigma, fun='gauss'):
+    '''
+    This function build the simiarity matrix between the columns of M according to some distance
+    '''
+    N = M.shape[1]
+    if fun == 'gauss':
+        return np.exp(-squareform(pdist(np.transpose(M)))**2/(2*sigma**2))
+    if fun == 'cos':
+        out = np.zeros((N,N))
+        for i in range(N):
+            for j in range(i):
+                val = cosine_similarity(M[:,i],M[:,j])
+                out[i,j] = val
+                out[j,i] = val
+        print('similarities : ', out+ np.eye(N))
+        return out + np.eye(N)
+
+
+def build_graphs(W, graph_type, thresh):
+    '''
+    Build the graph from similarities
+    '''
+    if graph_type =='eps':
+        W[W < thresh] == 0
+        return W
+
+    if graph_type == 'knn':
+        N = W.shape[0]
+        for i in range(N):
+            W[i,i]=0
+            idx = W[i,:].argsort()[:-thresh]
+            W[i,idx]=0
+        return np.maximum(W,np.transpose(W))
+
+def SpectralClustering(Affinity, n_clusters, n_axis, nor_type):
+    """
+    Affinity : N by N affinity matrix, where N is the number of points.
+    n_clusters : number of groups
+    n_axis : number of eigenvectors to keep from the Laplacian
+    nor_type : 'unn' or 'nor' for an unnormalized or normalized Laplacian
+    labels : if provided, algorithm will show a 2-d spectral representation of the data,
+    colored according to the labels provided
+
+    """
+    W = Affinity
+    N = W.shape[0]
+    D = np.zeros((N,N))
+
+    #We build the Laplacian
+
+    if nor_type == 'unn':
+        for  i in range(N):
+            D[i,i] = sum(W[i,:])
+
+        L = D - W
+
+    elif nor_type == 'nor':
+        for  i in range(N):
+            D[i,i] = sum(W[i,:])**(-1/2)
+
+        L = np.eye(N) - D @ W @ D
+
+    #Compute eigenvectors
+    d,p = LA.eig(L)
+    idx = np.argsort(d)
+    d = d[idx]
+    p = p[:,idx]
+
+
+    Y = p[:,1:n_axis+1]
+
+    kmeans = KMeans(n_clusters = n_clusters).fit(Y)
+
+    return [d, Y , kmeans.labels_]
+
+
+def SC(X, sigma, graph_type, thresh, n_clusters, n_axis, nor_type, labels, plot=True):
+    '''
+    Performs spectral clustering
+    X : data
+    sigma : for the gaussian similarity
+    graph type: knn or eps
+    thresh : eps or k
+    n_clusters: number of groups
+    n_axis : number of eigenvectors to keep
+    labels: true label (for the plot)
+    plot: whether to plot fancy illustrations or not
+
+    '''
+
+    W = build_similarity(X, sigma, 'gauss')
+    Aff = build_graphs(W, graph_type, thresh)
+    output = SpectralClustering(Aff,n_clusters, n_axis, nor_type)
+    d = output[0]
+    #print('Clustering error : ' , clustering_error(labels, output[2]))
+
+    if plot == True:
+        pca = PCA(n_components=2)
+        proj_data = pca.fit_transform(X.T)
+        proj_spectral = pca.fit_transform(output[1])
+
+        f, ax = plt.subplots(2, 2, figsize=(30,15))
+        ax[0,0].scatter(proj_data[:, 0], proj_data[:, 1], c=labels, cmap='prism', linewidths = 0.5, s=50)
+        ax[0,0].set_title('Original data')
+        ax[0,1].scatter(range(n_axis + 5), d[:n_axis + 5], c='b', linewidths = 1, s=75)
+        ax[0,1].axvline(x = n_axis - 1/2, c='r', linewidth = 2 )
+        ax[0,1].set_title('Eigenvalues')
+        ax[1,0].scatter(proj_spectral[:, 0], proj_spectral[:, 1], c=labels, cmap='prism', linewidths = 0.5, s=50)
+        ax[1,0].set_title('Ground truth')
+        ax[1,1].scatter(proj_spectral[:, 0], proj_spectral[:, 1], c=output[2], cmap='prism', linewidths = 0.5, s=50)
+        ax[1,1].set_title('K-Means')
+
+
+
+    return output[1]
