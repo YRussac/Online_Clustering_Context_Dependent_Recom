ed6f9a252417035c841cfc9da47ef1da2f164c6d
Yoan Russac <russacyoan@gmail.com>
2018-01-03 13:26:46 +0100

    initialization parameters correction

--- generator.py
+++ generator.py
@@ -8,7 +8,7 @@ def sample_spherical(npoints, ndim=3):
     return vec
 
 
-def generate_data(T, n_users, n_products, d, method_users = 'blobs', method_products= 'blobs',
+def generate_data(T, n_users, n_products, d, method_users = 'blobs', method_products= 'One-Hot',
                   n_class_products = None, n_class_users = 4, context_len = 3,
                   users_cluster_param = 0.05, products_cluster_param = 0.01):
     '''
